4d88fc9843fd6c39d96104deea804427fbdcf159
vimbaiD7 <172248661+vimbaiD7@users.noreply.github.com>
2025-04-15 15:07:10 +0200

    TUTORIAL DONE

--- EnumerableAndDisposable/EnumerableTutorial/Program.cs
+++ EnumerableAndDisposable/EnumerableTutorial/Program.cs
@@ -1,6 +1,22 @@
 ﻿//Your code here (above the PersonName class)
+using System.Collections;
+using System.Collections.Generic;
 
-public class PersonName
+
+/*var bond = new PersonName("James", "Bond");
+foreach (var name in bond)
+{
+    Console.WriteLine(name);
+}*/
+var rock = new PersonName("Dwayne", "Johnson", "Rock");
+Console.WriteLine(rock.Skip(1).First());
+
+/*var bondEnumerator = bond.GetEnumerator();
+while (bondEnumerator.MoveNext()) // using while
+{
+    Console.WriteLine(bondEnumerator.Current);
+}*/
+public class PersonName: IEnumerable<string>
 {
     public string FirstName { get; }
     public string MiddleName { get; }
@@ -12,4 +28,45 @@ public class PersonName
         MiddleName = middleName;
         LastName = lastName;
     }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerator<string> GetEnumerator()
+    {
+       // return new NameEnumerator(this);
+       yield return FirstName; // using yield, no need to create separate class the implements IEnumerator<string>
+        if (MiddleName != null)
+            yield return MiddleName;
+
+        yield return LastName;
+    }
+
+   /* public class NameEnumerator : IEnumerator<string>
+    {
+        private readonly PersonName _person;
+        private int _currentIndex = -1;
+
+        public NameEnumerator(PersonName person) => _person = person;
+
+        public bool MoveNext()
+        {
+            _currentIndex++;
+            return _person.MiddleName != null && _currentIndex < 3 || _person.MiddleName == null && _currentIndex < 2;
+        }
+
+        public void Reset() => _currentIndex = -1;
+
+        public string Current
+        {
+            get
+            {
+                if(_currentIndex == 0) return _person.FirstName;
+                if (_currentIndex == 1 && _person.MiddleName !=  null) return _person.MiddleName;
+                return _person.LastName;
+            }
+        }
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+    }*/
+
 }
--- EnumerableAndDisposable/FileBrowser/FileBrowserForm.cs
+++ EnumerableAndDisposable/FileBrowser/FileBrowserForm.cs
@@ -1,27 +1,61 @@
+using System.IO;
+
 namespace FileBrowser;
 
 public partial class FileBrowserForm : Form
 {
+    private FilePreviewer _filePreviewer;
     public FileBrowserForm()
     {
         InitializeComponent();
+        nextButton.Enabled = false;
+        closeButton.Enabled = false;
     }
 
     private void openButton_Click(object sender, EventArgs e)
     {
         if (openFileDialog.ShowDialog() == DialogResult.OK)
         {
+            /*using var streamReader = new StreamReader(openFileDialog.FileName);
+            resultTextBox.Text = streamReader.ReadLine();
+            var streamReader = new StreamReader(openFileDialog.FileName);
 
+            resultTextBox.Text = streamReader.ReadLine();
+            streamReader.Dispose();*///calling dispose manually
+            _filePreviewer = new FilePreviewer(openFileDialog.FileName);
+            openButton.Enabled = false;
+            nextButton.Enabled = true;
+            closeButton.Enabled = true;
         }
     }
 
     private void nextButton_Click(object sender, EventArgs e)
     {
-
+        resultTextBox.Text = _filePreviewer.GetNextLine();
     }
 
     private void closeButton_Click(object sender, EventArgs e)
     {
-
+        _filePreviewer.Dispose();
+        openButton.Enabled = true;
+        nextButton.Enabled = false;
+        resultTextBox.Clear();
+    }
+}
+public class FilePreviewer : IDisposable
+{
+    private readonly StreamReader _reader;
+    public FilePreviewer(string path) => _reader = new StreamReader(path);
+    public string GetNextLine()
+    {
+        if (_reader.EndOfStream)
+        {
+            return "--END OF FILE--";
+        }
+        return _reader.ReadLine();
+    }
+    public void Dispose()
+    {
+        _reader.Dispose();
     }
 }
